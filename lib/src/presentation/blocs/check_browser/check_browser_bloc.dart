e27519c9dc58af731f39d2f9667023eb736563de
Sarawut Saengchan <Sarawut.Saengchan@krungsri.com>
2023-09-06 16:35:35 +0700

    [SAMP-657][ namo ] add check borwser unit test

--- lib/src/presentation/blocs/check_browser/check_browser_bloc.dart
+++ lib/src/presentation/blocs/check_browser/check_browser_bloc.dart
@@ -3,27 +3,23 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:flutter_line_liff/flutter_line_liff.dart';
 import 'package:universal_html/html.dart';
 
 part 'check_browser_event.dart';
+
 part 'check_browser_state.dart';
 
 class CheckBrowserBloc extends Bloc<CheckBrowserEvent, CheckBrowserState> {
-  final liff = FlutterLineLiff();
   CheckBrowserBloc() : super(CheckBrowserInitial()) {
     on<GetBrowserClient>((event, emit) async {
       emit(CheckBrowserLoading());
-      await liff.ready.then((_) async {
-        // var userAgent = js.context['navigator']['userAgent'];
-        var userAgent = window.navigator.userAgent;
-        bool isLineBrowser = userAgent.contains('Line');
-        if (!liff.isInClient && !isLineBrowser) {
-          emit(BrowserIsNotLineLiff());
-        } else {
-          emit(BrowserIsLineLiff());
-        }
-      });
+      var userAgent = window.navigator.userAgent;
+      bool isLineBrowser = userAgent.contains('Line');
+      if (!isLineBrowser) {
+        emit(BrowserIsNotLineLiff());
+      } else {
+        emit(BrowserIsLineLiff());
+      }
     });
   }
 }
