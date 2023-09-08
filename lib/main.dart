50cbb9fcb62563072597dbbc9a7391aa08f6c089
Prem.natthapong <natthapong.tantecho@krungsri.com>
2023-09-08 10:45:53 +0700

    [SAMP-664][X][Prem]Implement RUM

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'dart:html';
-
 import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:datadog_flutter_plugin/datadog_flutter_plugin.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -13,9 +13,28 @@ import 'package:marketplace_line_oa/src/presentation/blocs/connectivity_status/c
 import 'package:marketplace_line_oa/src/routes/routes.dart';
 // import 'configure_nonweb.dart' if (dart.library.html) 'configure_web.dart';
 
+late DdSdkConfiguration configuration;
+
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  _loadENV();
+  _configureApp();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]).then((_) async {
+    usePathUrlStrategy();
+    await DatadogSdk.runApp(configuration, () async {
+      runApp(const MyApp());
+    });
+  });
+}
+
+_configureApp() {
+  _setUpDatadog(); //get ENV also implemented here.
+  _setUpLineLIFF();
+}
+
+_setUpLineLIFF() {
   LineDataHelper lineDataHelper = LineDataHelper();
 
   if (Uri.base.queryParameters.isNotEmpty) {
@@ -29,29 +48,30 @@ void main() {
     Storage localStorage = window.localStorage;
     localStorage.addAll({"LineLogin": 'true'});
   }
-
+  const env = String.fromEnvironment('SET_ENV', defaultValue: 'dev');
   FlutterLineLiff().init(
-      config: Config(liffId: '1661164508-Kn9nO7oB'),
+      //TODO: config LIFF for prod
+      config: env == "prod" ? Config(liffId: '1661164508-Kn9nO7oB') : Config(liffId: '1661164508-Kn9nO7oB'),
       successCallback: () {
         print('successCallback');
       },
       errorCallback: (error) {
         print('init error: ${error.name}, ${error.message}, ${error.stack}');
       });
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown,
-  ]).then((_) async {
-    // configureApp();
-    usePathUrlStrategy();
-    runApp(const MyApp());
-  });
 }
 
-_loadENV() {
-  const setEnv = String.fromEnvironment('SET_ENV', defaultValue: 'dev');
-  String? env = setEnv;
-  print('env : $env');
+_setUpDatadog() {
+  configuration = DdSdkConfiguration(
+    clientToken: 'pub002fb557c4b3f796b2eb3e9a2cc3bcdd',
+    env: const String.fromEnvironment('SET_ENV', defaultValue: 'dev'),
+    site: DatadogSite.us1,
+    trackingConsent: TrackingConsent.granted,
+    nativeCrashReportEnabled: true,
+    loggingConfiguration: LoggingConfiguration(),
+    rumConfiguration: RumConfiguration(
+      applicationId: '93edfddb-2127-4074-b50c-ae8d9b9fadee'
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -107,6 +127,9 @@ class _RootPageState extends State<RootPage> {
         scaffoldBackgroundColor: const Color.fromARGB(255, 172, 204, 229),
         appBarTheme: const AppBarTheme(backgroundColor: Colors.white, foregroundColor: Color(0xff2c2626)),
       ),
+      navigatorObservers: [
+        DatadogNavigationObserver(datadogSdk: DatadogSdk.instance),
+      ],
     );
   }
 }
