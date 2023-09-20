94a3c110563948fca299c33868c06dbbe860e5fc
Arayaphong Traisopon <ks-auto-bay+gocd-agent-user@thoughtworks.com>
2023-09-20 09:03:26 +0700

    [SAMP-583][Arme] try to make tabBar work but still not

--- lib/src/presentation/screens/home.dart
+++ lib/src/presentation/screens/home.dart
@@ -26,7 +26,7 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
   // void webCookiePolicyBTS() {
   //   var maxWidth = MediaQuery.of(context).size.width;
   //   // เรียกใช้ showModalBottomSheet
@@ -60,109 +60,221 @@ class _HomeScreenState extends State<HomeScreen> {
 
   showLoading(BuildContext context) {
     GeneralDialog().showLoadingDialog(context: context);
-    Future.delayed(const Duration(seconds: 10)).then((value) => Navigator.pop(context));
+    Future.delayed(const Duration(seconds: 10))
+        .then((value) => Navigator.pop(context));
   }
 
-  PageController pageController = PageController(initialPage: 0, keepPage: false);
+  PageController pageController =
+      PageController(initialPage: 0, keepPage: false);
 
   @override
   Widget build(BuildContext context) {
-    var maxWidth = MediaQuery.of(context).size.width;
+    final size = MediaQuery.of(context).size;
+    final maxWidth = size.width;
+    final tabController = TabController(length: 4, vsync: this);
     return RootPageCondition(
         child: AlvaRootWidget(
             titlePage: titleWebPage,
             child: Container(
               color: cloudyWhite,
-              child: ListView(
-                children: [
-                  HomepageTopSection(maxWidth: maxWidth),
-                  HomePageBanner(
-                    maxWidth: maxWidth,
-                    pageControllerState: pageController,
+              child: CustomScrollView(
+                clipBehavior: Clip.none,
+                slivers: [
+                  SliverList(
+                      delegate: SliverChildListDelegate.fixed([
+                    HomepageTopSection(maxWidth: maxWidth),
+                    HomePageBanner(
+                      maxWidth: maxWidth,
+                      pageControllerState: pageController,
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                  ])),
+                  const SliverToBoxAdapter(
+                    child: SizedBox(
+                      height: 16,
+                    ),
                   ),
-                  const SizedBox(
-                    height: 8,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      SizedBox(
-                        width: maxWidth - 32,
-                        child: ProductCardWidget(maxWidth: maxWidth),
-                      ),
-                    ],
-                  ),
-                  Column(
-                    children: [
-                      Container(
-                        color: cloudDeepWhite,
-                        height: 32,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.end,
-                          children: [
-                            AlvaText(
-                              title: HomeConst().termsAndConditions,
-                              textStyle: AlvaStyles().headingSize10(),
-                            ),
-                            Container(
-                              margin: const EdgeInsets.symmetric(horizontal: 8),
-                              width: 1,
-                              height: 16,
-                              color: cloudSoftDeepWhite,
-                            ),
-                            AlvaText(
-                              title: HomeConst().privacyPolicy,
-                              textStyle: AlvaStyles().headingSize10(),
-                            ),
-                          ],
+                  SliverList(
+                      delegate: SliverChildListDelegate.fixed([
+                    TabBar(
+                      controller: tabController,
+                      tabs: const <Widget>[
+                        Tab(
+                          icon: Icon(Icons.cloud_outlined),
+                          // icon: Image.asset("assets/images/category/icon_cate_all.png"),
+                          text: "ทั้งหมด",
                         ),
-                      ),
-                      Container(
-                        color: cloudDeepWhite,
-                        height: 72,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                AlvaText(
-                                  title: HomeConst().askInformation,
-                                  textStyle: AlvaStyles().headingSize10(),
-                                ),
-                              ],
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                AlvaText(
-                                  title: HomeConst().pleaseContact,
-                                  textStyle: AlvaStyles().headingSize12w700(sugarRed),
-                                ),
-                              ],
-                            ),
-                          ],
+                        Tab(
+                          icon: Icon(Icons.beach_access_sharp),
+                          text: "วอลชาร์จ",
                         ),
-                      ),
-                      Container(
-                        color: spaceGrey,
-                        height: 40,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            AlvaText(
-                              title: HomeConst().warningWord,
-                              textStyle: AlvaStyles().headingSize10w400(whiteFalse),
-                            ),
-                          ],
+                        Tab(
+                          icon: Icon(Icons.brightness_5_sharp),
+                          text: "โซลาร์เซลล์",
                         ),
-                      ),
-                    ],
+                        Tab(
+                          icon: Icon(Icons.access_alarm_sharp),
+                          text: "สินค้าอื่นๆ",
+                        ),
+                      ],
+                    )
+                  ])),
+                  SliverFillRemaining(
+                    child: TabBarView(
+                      clipBehavior: Clip.none,
+                      controller: tabController,
+                      children: <Widget>[
+                        // ProductCardWidget(maxWidth: maxWidth),
+                        Text("It's qwer here"),
+                        Text("It's rainy here"),
+                        Text("It's sunny here"),
+                        Text("It's asdf here"),
+                      ],
+                    ),
+
+                    // child: Column(
+                    //   mainAxisSize: MainAxisSize.min,
+                    //   crossAxisAlignment: CrossAxisAlignment.stretch,
+                    //   children: <Widget>[
+                    //     Expanded(
+                    //       child: TabBarView(
+                    //           clipBehavior: Clip.none,
+                    //           controller: tabController,
+                    //           children: [
+                    //             ProductCardWidget(maxWidth: maxWidth),
+                    //             // Text("It's qwer here"),
+                    //             Text("It's rainy here"),
+                    //             Text("It's sunny here"),
+                    //             Text("It's asdf here"),
+                    //           ]),
+                    //     )
+                    //   ],
+                    // ),
                   ),
+                  SliverList(
+                      delegate: SliverChildListDelegate.fixed([
+                    Column(
+                      children: [
+                        Container(
+                          color: cloudDeepWhite,
+                          height: 32,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.end,
+                            children: [
+                              AlvaText(
+                                title: HomeConst().termsAndConditions,
+                                textStyle: AlvaStyles().headingSize10(),
+                              ),
+                              Container(
+                                margin:
+                                    const EdgeInsets.symmetric(horizontal: 8),
+                                width: 1,
+                                height: 16,
+                                color: cloudSoftDeepWhite,
+                              ),
+                              AlvaText(
+                                title: HomeConst().privacyPolicy,
+                                textStyle: AlvaStyles().headingSize10(),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          color: cloudDeepWhite,
+                          height: 72,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  AlvaText(
+                                    title: HomeConst().askInformation,
+                                    textStyle: AlvaStyles().headingSize10(),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  AlvaText(
+                                    title: HomeConst().pleaseContact,
+                                    textStyle: AlvaStyles()
+                                        .headingSize12w700(sugarRed),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          color: spaceGrey,
+                          height: 40,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              AlvaText(
+                                title: HomeConst().warningWord,
+                                textStyle:
+                                    AlvaStyles().headingSize10w400(whiteFalse),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ]))
                 ],
               ),
             )));
+  }
+}
+
+// Poc appbar
+class TabBarApp extends StatelessWidget {
+  const TabBarApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: const TabBarExample(),
+    );
+  }
+}
+
+class TabBarExample extends StatelessWidget {
+  const TabBarExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      initialIndex: 1,
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: const TabBar(
+            tabs: <Widget>[
+              Tab(
+                icon: Icon(Icons.cloud_outlined),
+              ),
+              Tab(
+                icon: Icon(Icons.beach_access_sharp),
+              ),
+              Tab(
+                icon: Icon(Icons.brightness_5_sharp),
+              ),
+            ],
+          ),
+        ),
+        // body: Expanded(
+        //   child: ,
+        // ),
+      ),
+    );
   }
 }
