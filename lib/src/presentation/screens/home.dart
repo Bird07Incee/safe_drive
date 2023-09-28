b8e060a96866c2d9e057029dd0a12f2da87014e9
Nuttawut <thisnattawut@gmail.com>
2023-09-28 14:56:54 +0700

    [SAMP-583][Pung, Arme] dynamic tab

--- lib/src/presentation/screens/home.dart
+++ lib/src/presentation/screens/home.dart
@@ -32,6 +32,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
   PageController pageController = PageController(initialPage: 0, keepPage: false);
+  late TabController tabController;
 
   @override
   void initState() {
@@ -63,7 +64,7 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     final maxWidth = size.width;
-    final tabController = TabController(length: 4, vsync: this);
+    // final tabController = TabController(length: 4, vsync: this);
     return RootPageCondition(
         child: AlvaRootWidget(
             titlePage: titleWebPage,
@@ -77,6 +78,8 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
               builder: (context, state) {
                 return BlocBuilder<ProductListBloc, ProductListState>(
                   builder: (context, state) {
+                    tabController = TabController(length: state.productList.category!.length, vsync: this);
+
                     if (state.productListStatus == GetProductListStatus.success) {
                       return Container(
                         color: cloudyWhite,
