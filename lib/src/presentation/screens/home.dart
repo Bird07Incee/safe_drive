71a87a9bb5182eca55cf1f24dcb86ce07999e96a
Nuttawut <thisnattawut@gmail.com>
2023-09-29 12:25:28 +0700

    [SAMP-583][Pung, Arme] debug

--- lib/src/presentation/screens/home.dart
+++ lib/src/presentation/screens/home.dart
@@ -98,16 +98,15 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
                                     controller: tabController,
                                     labelColor: Colors.black,
                                     indicatorColor: BlueFantasy,
+                                    // isScrollable: state.productList.category!.length <= 3 ? false : true ,
                                     isScrollable: true,
                                     labelStyle: AlvaStyles().headingSize10w600(BTN_SELECTED_TEXT_COLOR_NEW),
                                     unselectedLabelColor: const Color(0xffDEDEDE),
                                     onTap: (int index) {
-                                      print("tab index : $index");
                                       context.read<ProductListBloc>().add(SetSelectTabIndex(index));
                                       if (index == 0) {
                                         context.read<ProductListBloc>().add(const GetProductList());
                                       } else {
-                                        print(state.productList.category![index - 1]["categoryId"]);
                                         context.read<ProductListBloc>().add(GetProductListByCategory(
                                             state.productList.category![index - 1]["categoryId"]));
                                       }
