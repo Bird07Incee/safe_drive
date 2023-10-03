e6af422171c21e5346da214a17b06384faadc877
Prem.natthapong <natthapong.tantecho@krungsri.com>
2023-10-03 15:34:32 +0700

    [SAMP-710][Prem, P'Kan]migrate pass arguments to bloc state (missing file product_detail_screen.dart)

--- lib/src/presentation/screens/product_detail/product_detail_screen.dart
+++ lib/src/presentation/screens/product_detail/product_detail_screen.dart
@@ -72,39 +72,47 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> with TickerPr
   int indicator = 1;
   double _scale = 1.0;
   String pid = '';
+  late Product product;
+  late Widget topSection = SizedBox();
+  late Widget bottomSection = SizedBox();
 
   //List dataCarouselMock = carouselSingleItem;
   @override
   Widget build(BuildContext context) {
     int imageDataLength = 1;
-    settings = ModalRoute.of(context) != null ? ModalRoute.of(context)!.settings : null;
-    if (widget.arguments != null) {
-      imageDataLength = widget.arguments!.product.productionAssets.length == 1
-          ? widget.arguments!.product.productionAssets.length
-          : widget.arguments!.product.productionAssets.length > 20
-              ? 20
-              : widget.arguments!.product.productionAssets.length;
-    } else {
-      if (settings != null) {
-        var uriData = Uri.parse(settings!.name!);
-        var routingData = RoutingData(route: uriData.path, queryParameters: uriData.queryParameters);
-        if (settings!.name == "/productDetail") {
-          pid = (routingData["pid"] == null) ? "" : routingData["pid"];
-        }
-        print(pid);
-      }
-    }
     maxWidth = MediaQuery.of(context).size.width;
     maxHeight = MediaQuery.of(context).size.height;
+    settings = ModalRoute.of(context) != null ? ModalRoute.of(context)!.settings : null;
+    if (settings != null) {
+      var uriData = Uri.parse(settings!.name!);
+      var routingData = RoutingData(route: uriData.path, queryParameters: uriData.queryParameters);
+      if (routingData.route == "/productDetail") {
+        pid = (routingData["pid"] == null) ? "" : routingData["pid"];
+      }
+      print("routingData route: ${routingData.route}");
+      print("routingData queryParams: ${routingData.queryParameters}");
+      print("settings!.name : ${settings!.name}");
+      print("pid : $pid");
+    }
     return RootPageCondition(
       child: BlocConsumer<ProductDetailBloc, ProductDetailState>(
         listener: (context, state) {
-          if ((widget.arguments == null || widget.arguments?.product == Product.empty) && state.status.isInitial && pid != "") {
-            context.read<ProductDetailBloc>().add(GetProductByID(pid: pid));
+          if (state.status.isSuccess && pid != "") {
+            product = state.product;
+            print('get product success');
+            print('product id : ${product.productId}, productName: ${product.productName}');
+            imageDataLength = product.productionAssets.length == 1
+                ? product.productionAssets.length
+                : product.productionAssets.length > 20
+                ? 20
+                : product.productionAssets.length;
           }
         },
-        builder: (context, state) {
-          if (state.status.isSuccess) {
+        builder: (context, pdState) {
+          if (pdState.status.isInitial && pid != "") {
+            context.read<ProductDetailBloc>().add(GetProductByID(pid: pid));
+          }
+          if (pdState.status.isSuccess) {
             return BlocBuilder<ScrollProductDetailBloc, ScrollProductDetailState>(
               builder: (ctx, stateAppBar) {
                 return BlocBuilder<ImgGalleryZoomBloc, TransformationController>(
@@ -128,7 +136,8 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> with TickerPr
                 );
               },
             );
-          } else if (state.status.isLoading) {
+          }
+          else if (pdState.status.isLoading) {
             return const LoadingScreen();
           } else {
             return ErrorScreen(
@@ -136,7 +145,7 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> with TickerPr
               subTitle: ErrorConst().subTitleNS,
               titleBtn: ErrorConst().titleBtnNS,
               onTap: () {
-                if ((widget.arguments == null || widget.arguments?.product == Product.empty) && state.status.isInitial && pid != "") {
+                if (pdState.status.isInitial && pid != "") {
                   context.read<ProductDetailBloc>().add(GetProductByID(pid: pid));
                 }
               },
@@ -148,8 +157,7 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> with TickerPr
     );
   }
 
-  WillPopScope productDetailPage(
-      ScrollProductDetailState stateAppBar, BuildContext context, PageController carouselState, int imageDataLength) {
+  WillPopScope productDetailPage(ScrollProductDetailState stateAppBar, BuildContext context, PageController carouselState, int imageDataLength) {
     return WillPopScope(
       onWillPop: () async {
         Navigator.pop(context);
@@ -157,12 +165,69 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> with TickerPr
         context.read<ScrollProductDetailBloc>().add(ProductDetailScrollAction(0, context, "1"));
         return true;
       },
-      child: AlvaRootWidget(
-          titlePage: titleWebPage,
-          appBar: stateAppBar.appBarCarDetailStatus
-              ? AppBar(
-                  automaticallyImplyLeading: false,
-                  leading: IconButton(
+      child: BlocBuilder<ProductDetailBloc, ProductDetailState>(
+        builder: (context, state) {
+          return AlvaRootWidget(
+              titlePage: titleWebPage,
+              appBar: stateAppBar.appBarCarDetailStatus
+                  ? AppBar(
+                automaticallyImplyLeading: false,
+                leading: IconButton(
+                  key: const Key("pop_navigator_to_home_page"),
+                  onPressed: () {
+                    Navigator.pop(context);
+                    context
+                        .read<ProductDetailCarouselScrollControllerBloc>()
+                        .add(const CarouselScrollAction(index: 0));
+                    context.read<ScrollProductDetailBloc>().add(ProductDetailScrollAction(0, context, "1"));
+                  },
+                  icon: const Icon(Icons.arrow_back_ios_rounded),
+                ),
+                leadingWidth: 60,
+                titleSpacing: 0,
+                title: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    AlvaTextMaxLinesOverflow(
+                        title: state.product.productName,
+                        maxLines: 1,
+                        textStyle: AlvaStyles()
+                            .headingSize12w600(BTN_SELECTED_TEXT_COLOR_NEW)
+                            .copyWith(fontWeight: FontWeight.w500, height: 1.17)),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        AlvaText(
+                            title: state.product.price.toDecimalFormat(),
+                            textStyle: AlvaStyles().heading1().copyWith(
+                                color: state.product.discountPrice > 0
+                                    ? RedWordShow
+                                    : BTN_SELECTED_TEXT_COLOR_NEW,
+                                height: 1.33)),
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 1),
+                          child: AlvaText(
+                              title: ' บาท',
+                              textStyle: AlvaStyles()
+                                  .heading2(state.product.discountPrice > 0
+                                  ? RedWordShow
+                                  : BTN_SELECTED_TEXT_COLOR_NEW)
+                                  .copyWith(height: 1.33)),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+                centerTitle: false,
+              )
+                  : AppBar(
+                title: AlvaText(
+                    title: "ข้อมูลสินค้า", textStyle: AlvaStyles().headingSize18w700(BTN_SELECTED_TEXT_COLOR_NEW)),
+                titleSpacing: 0,
+                leadingWidth: 60,
+                centerTitle: false,
+                automaticallyImplyLeading: false,
+                leading: IconButton(
                     key: const Key("pop_navigator_to_home_page"),
                     onPressed: () {
                       Navigator.pop(context);
@@ -171,113 +236,60 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> with TickerPr
                           .add(const CarouselScrollAction(index: 0));
                       context.read<ScrollProductDetailBloc>().add(ProductDetailScrollAction(0, context, "1"));
                     },
-                    icon: const Icon(Icons.arrow_back_ios_rounded),
-                  ),
-                  leadingWidth: 60,
-                  titleSpacing: 0,
-                  title: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      AlvaTextMaxLinesOverflow(
-                          title: widget.arguments!.product.productName,
-                          maxLines: 1,
-                          textStyle: AlvaStyles()
-                              .headingSize12w600(BTN_SELECTED_TEXT_COLOR_NEW)
-                              .copyWith(fontWeight: FontWeight.w500, height: 1.17)),
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          AlvaText(
-                              title: widget.arguments!.product.price.toDecimalFormat(),
-                              textStyle: AlvaStyles().heading1().copyWith(
-                                  color: widget.arguments!.product.discountPrice > 0
-                                      ? RedWordShow
-                                      : BTN_SELECTED_TEXT_COLOR_NEW,
-                                  height: 1.33)),
-                          Padding(
-                            padding: const EdgeInsets.only(bottom: 1),
-                            child: AlvaText(
-                                title: ' บาท',
-                                textStyle: AlvaStyles()
-                                    .heading2(widget.arguments!.product.discountPrice > 0
-                                        ? RedWordShow
-                                        : BTN_SELECTED_TEXT_COLOR_NEW)
-                                    .copyWith(height: 1.33)),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                  centerTitle: false,
-                )
-              : AppBar(
-                  title: AlvaText(
-                      title: "ข้อมูลสินค้า", textStyle: AlvaStyles().headingSize18w700(BTN_SELECTED_TEXT_COLOR_NEW)),
-                  titleSpacing: 0,
-                  leadingWidth: 60,
-                  centerTitle: false,
-                  automaticallyImplyLeading: false,
-                  leading: IconButton(
-                      key: const Key("pop_navigator_to_home_page"),
-                      onPressed: () {
-                        Navigator.pop(context);
-                        context
-                            .read<ProductDetailCarouselScrollControllerBloc>()
-                            .add(const CarouselScrollAction(index: 0));
-                        context.read<ScrollProductDetailBloc>().add(ProductDetailScrollAction(0, context, "1"));
-                      },
-                      icon: const Icon(Icons.arrow_back_ios_rounded)),
+                    icon: const Icon(Icons.arrow_back_ios_rounded)),
+              ),
+              bottomSheet: Container(
+                color: whitePure,
+                width: maxWidth,
+                height: 96,
+                padding: const EdgeInsets.only(left: 16, right: 16, bottom: 32, top: 16),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(borderRadius: BorderRadius.circular(8)),
+                        height: 48,
+                        child: OutlinedButton(
+                          onPressed: () {
+                            Navigator.pushNamed(context,
+                                '${Routes.selectOptions.toStringPath()}?pid=${state.product.productId}',
+                                arguments: ProductDetailArgs(product: state.product));
+                          },
+                          style: AlvaStyles()
+                              .outlineNoneBorderButtonStyle(YellowKrungsri, Colors.transparent, isRadius8: true),
+                          child: Text("สั่งซื้อสินค้า", style: AlvaStyles().headingSize16w700(BTN_SELECTED_TEXT_COLOR_NEW)),
+                        ),
+                      ),
+                    )
+                  ],
                 ),
-          bottomSheet: Container(
-            color: whitePure,
-            width: maxWidth,
-            height: 96,
-            padding: const EdgeInsets.only(left: 16, right: 16, bottom: 32, top: 16),
-            child: Row(
-              children: [
-                Expanded(
-                  child: Container(
-                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(8)),
-                    height: 48,
-                    child: OutlinedButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context,
-                            '${Routes.selectOptions.toStringPath()}?pid=${widget.arguments!.product.productId}',
-                            arguments: ProductDetailArgs(product: widget.arguments!.product));
-                      },
-                      style: AlvaStyles()
-                          .outlineNoneBorderButtonStyle(YellowKrungsri, Colors.transparent, isRadius8: true),
-                      child: Text("สั่งซื้อสินค้า", style: AlvaStyles().headingSize16w700(BTN_SELECTED_TEXT_COLOR_NEW)),
-                    ),
-                  ),
-                )
-              ],
-            ),
-          ),
-          // child: ProductDetailBody(),
-          child: Container(
-            padding: const EdgeInsets.only(bottom: 96),
-            color: backgroundNo2,
-            child: ListView(
-              shrinkWrap: true,
-              controller: scrollController,
-              children: [
-                widget.arguments != null ? PDTopSection(args: widget.arguments!) : SizedBox(),
-                Container(
-                  height: 16,
-                  decoration: BoxDecoration(
-                    border: Border(
-                      top: BorderSide(
-                        color: cloudWhite, // Replace with your color
-                        width: 2.0, // Adjust the border width as needed
+              ),
+              // child: ProductDetailBody(),
+              child: Container(
+                padding: const EdgeInsets.only(bottom: 96),
+                color: backgroundNo2,
+                child: ListView(
+                  shrinkWrap: true,
+                  controller: scrollController,
+                  children: [
+                    PDTopSection(),
+                    Container(
+                      height: 16,
+                      decoration: BoxDecoration(
+                        border: Border(
+                          top: BorderSide(
+                            color: cloudWhite, // Replace with your color
+                            width: 2.0, // Adjust the border width as needed
+                          ),
+                        ),
                       ),
                     ),
-                  ),
+                  PDBottomSection()
+                  ],
                 ),
-                widget.arguments != null ? PDBottomSection(args: widget.arguments!) : SizedBox()
-              ],
-            ),
-          )),
+              ));
+        },
+      ),
     );
   }
 
@@ -355,21 +367,25 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> with TickerPr
                                 child: SizedBox(
                                   width: maxWidth,
                                   height: 576,
-                                  child: FadeInImage(
-                                    placeholder: AssetImage(ProductDetailConst().imgDefaultPath),
-                                    image: NetworkImage(
-                                      i == imageDataLength
-                                          ? widget.arguments!.product.productionAssets[0]
-                                          : widget.arguments!.product.productionAssets[i],
-                                    ),
-                                    // image: NetworkImage(
-                                    //   i == imageDataLength
-                                    //       ? dataCarouselMock[0].substring(46)
-                                    //       : dataCarouselMock[i].substring(46),
-                                    // ),
-                                    fit: BoxFit.fitWidth,
-                                    imageErrorBuilder: (context, error, stackTrace) =>
-                                        Image.asset(ProductDetailConst().imgDefaultPath, fit: BoxFit.fitWidth),
+                                  child: BlocBuilder<ProductDetailBloc, ProductDetailState>(
+                                    builder: (context, state) {
+                                      return FadeInImage(
+                                        placeholder: AssetImage(ProductDetailConst().imgDefaultPath),
+                                        image: NetworkImage(
+                                          i == imageDataLength
+                                              ? state.product.productionAssets[0]
+                                              : state.product.productionAssets[i],
+                                        ),
+                                        // image: NetworkImage(
+                                        //   i == imageDataLength
+                                        //       ? dataCarouselMock[0].substring(46)
+                                        //       : dataCarouselMock[i].substring(46),
+                                        // ),
+                                        fit: BoxFit.fitWidth,
+                                        imageErrorBuilder: (context, error, stackTrace) =>
+                                            Image.asset(ProductDetailConst().imgDefaultPath, fit: BoxFit.fitWidth),
+                                      );
+                                    },
                                   ),
                                 ),
                               );
