9d3bb01fa4a994f9f3c6886fff36022a587495f8
Sarawut Saengchan <Sarawut.Saengchan@krungsri.com>
2023-10-04 14:22:32 +0700

    [SAMP-563][namo] [add scope] delete import not use

--- lib/src/presentation/screens/product_select_options.dart
+++ lib/src/presentation/screens/product_select_options.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:marketplace_line_oa/src/constants/alva_styles.dart';
