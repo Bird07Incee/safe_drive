96dcf769048a63b8640dfb7710e603e2bb6fd19a
Visaruth Punnium <visaruth.punnium@krungsri.com>
2023-10-16 09:32:38 +0700

    [SAMP-670][web1.0][Meg] fix root page and add push name

--- lib/src/presentation/screens/read_term_and_con.dart
+++ lib/src/presentation/screens/read_term_and_con.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:marketplace_line_oa/src/constants/alva_styles.dart';
 import 'package:marketplace_line_oa/src/constants/mkp_styles.dart';
 import 'package:marketplace_line_oa/src/constants/my_constants.dart';
+import 'package:marketplace_line_oa/src/presentation/screens/root_page_condition.dart';
 import 'package:marketplace_line_oa/src/presentation/widget/alva_text.dart';
 import 'package:marketplace_line_oa/src/presentation/widget/root_widget.dart';
 import 'package:marketplace_line_oa/src/presentation/widget/term_and_con/term_and_con_section.dart';
@@ -14,33 +15,38 @@ class ReadTermAndConScreen extends StatelessWidget {
     final maxWidth = MediaQuery.of(context).size.width;
     final maxHeight = MediaQuery.of(context).size.height - 56;
 
-    return WillPopScope(
-      onWillPop: () async => false,
-      child: AlvaRootWidget(
-          titlePage: titleWebPage,
-          child: Column(
-            children: [
-              AppBar(
-                title: AlvaText(
-                    title: "ข้อกำหนดและเงื่อนไข",
-                    textStyle: AlvaStyles().headingSize18w700(BTN_SELECTED_TEXT_COLOR_NEW)),
-                titleSpacing: 0,
-                leadingWidth: 60,
-                centerTitle: false,
-                automaticallyImplyLeading: false,
-                leading: IconButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    icon: const Icon(Icons.arrow_back_ios_rounded)),
-              ),
-              Container(
-                  width: maxWidth,
-                  height: maxHeight,
-                  color: const Color(0xfff3f3f3),
-                  child: ListView(children: const [TermAndConSection()]))
-            ],
-          )),
+    return RootPageCondition(
+      child: WillPopScope(
+        onWillPop: () async {
+          Navigator.pushNamed(context, '/');
+          return true;
+        },
+        child: AlvaRootWidget(
+            titlePage: titleWebPage,
+            child: Column(
+              children: [
+                AppBar(
+                  title: AlvaText(
+                      title: "ข้อกำหนดและเงื่อนไข",
+                      textStyle: AlvaStyles().headingSize18w700(BTN_SELECTED_TEXT_COLOR_NEW)),
+                  titleSpacing: 0,
+                  leadingWidth: 60,
+                  centerTitle: false,
+                  automaticallyImplyLeading: false,
+                  leading: IconButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/');
+                      },
+                      icon: const Icon(Icons.arrow_back_ios_rounded)),
+                ),
+                Container(
+                    width: maxWidth,
+                    height: maxHeight,
+                    color: const Color(0xfff3f3f3),
+                    child: ListView(children: const [TermAndConSection()]))
+              ],
+            )),
+      ),
     );
   }
 }
