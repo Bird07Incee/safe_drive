5dfc85e065da1355610a8438a335a96c7465614b
Nuttawut <thisnattawut@gmail.com>
2023-10-17 13:31:27 +0700

    [SAMP-577][Pung] set image carousel page

--- lib/src/presentation/widget/homepage/product_card_widget.dart
+++ lib/src/presentation/widget/homepage/product_card_widget.dart
@@ -88,7 +88,7 @@ class ProductCardWidget extends StatelessWidget {
                               context.read<ViewImgDetailPageSwitchBloc>().add(SwitchPageAction(statePage: true));
                               context
                                   .read<ProductDetailCarouselScrollControllerBloc>()
-                                  .add(CarouselScrollAction(index: 0));
+                                  .add(CarouselScrollAction(index: activeIndex[index]));
                               context.read<PreviousScaleBloc>().add(const PreviousScaleEvent(previousScale: 0.5));
                               context.read<ProductDetailBloc>().add(SetClickFromImage(isClickFromImage: true));
 
