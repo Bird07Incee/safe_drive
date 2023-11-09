c43cc876388c1ee825b464dbd3c64afa9516c00a
Nuttawut <thisnattawut@gmail.com>
2023-11-09 16:11:42 +0700

    [SAMP-751][Pung] change price display logic

--- lib/src/presentation/widget/homepage/product_card_widget.dart
+++ lib/src/presentation/widget/homepage/product_card_widget.dart
@@ -305,7 +305,7 @@ class ProductCardWidget extends StatelessWidget {
                                       ),
                                     ),
                                     Visibility(
-                                      visible: products[index].discountPrice != 0,
+                                      visible: products[index].discountPrice != 0 && products[index].productionOptionals.isEmpty,
                                       child: Row(
                                         children: [
                                           Text(
@@ -330,7 +330,7 @@ class ProductCardWidget extends StatelessWidget {
                                             Text(
                                               intl.NumberFormat.decimalPattern()
                                                   .format(getDisplayPrice(products[index].price, products[index].productionOptionals)),
-                                              style: products[index].discountPrice == 0
+                                              style: products[index].discountPrice == 0 && products[index].productionOptionals.isNotEmpty
                                                   ? AlvaStyles().headingSize22(BTN_SELECTED_TEXT_COLOR_NEW)
                                                   : AlvaStyles().headingSize22(RedWordShow),
                                             ),
@@ -339,7 +339,7 @@ class ProductCardWidget extends StatelessWidget {
                                             ),
                                             Text(
                                               "บาท",
-                                              style: products[index].discountPrice == 0
+                                              style: products[index].discountPrice == 0 && products[index].productionOptionals.isNotEmpty
                                                   ? AlvaStyles().headingSize18(BTN_SELECTED_TEXT_COLOR_NEW)
                                                   : AlvaStyles().headingSize18(RedWordShow),
                                             ),
--- lib/src/utils/get_display_price.dart
+++ lib/src/utils/get_display_price.dart
@@ -2,9 +2,8 @@ import 'package:marketplace_line_oa/src/model/product_list.dart';
 
 int getDisplayPrice(int price, List<ProductionOptionals> option) {
   var result = price;
-  var optionList = option.toList();
 
-  if (optionList.isNotEmpty) {
+  if (option.isNotEmpty) {
     option.sort((a, b) => a.price.compareTo(b.price));
     result = option.first.price;
   }
