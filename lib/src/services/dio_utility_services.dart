7de62437d5afd43b0fa8e5c62699a250a6a71085
Prem.natthapong <natthapong.tantecho@krungsri.com>
2023-11-14 18:27:16 +0700

    [SAMP-X][Prem, P'kan] -Remove AWSRootCA from assets -Add certificate pinning in dio interceptor

--- lib/src/services/dio_utility_services.dart
+++ lib/src/services/dio_utility_services.dart
@@ -1,6 +1,9 @@
 import 'package:dio/dio.dart';
+import 'package:dio/io.dart';
+import 'package:flutter/services.dart';
 import 'package:marketplace_line_oa/src/helpers/dio_intercetptor.dart';
 import 'package:marketplace_line_oa/src/services/dio_utils/header_utils.dart';
+import 'package:universal_io/io.dart';
 
 class DioUtilityService with HeaderUtil {
   DioUtilityService({Dio? dio}) : _dioClient = dio ?? DioClient().dioClient;
@@ -70,7 +73,41 @@ class DioUtilityService with HeaderUtil {
 class DioClient {
   static final Dio client = Dio();
   Dio get dioClient {
+    client.interceptors.add(InterceptorsWrapper(
+      onRequest: (RequestOptions options, RequestInterceptorHandler handler) async {
+        initAdapter();
+        return handler.next(options);
+      },
+    ));
+
     client.interceptors.add(DioInterceptor());
     return client;
+  }
+
+  void initAdapter() {
+    String pemString = """
+  -----BEGIN CERTIFICATE-----
+MIIB8jCCAXigAwIBAgITBmyf18G7EEwpQ+Vxe3ssyBrBDjAKBggqhkjOPQQDAzA5
+MQswCQYDVQQGEwJVUzEPMA0GA1UEChMGQW1hem9uMRkwFwYDVQQDExBBbWF6b24g
+Um9vdCBDQSA0MB4XDTE1MDUyNjAwMDAwMFoXDTQwMDUyNjAwMDAwMFowOTELMAkG
+A1UEBhMCVVMxDzANBgNVBAoTBkFtYXpvbjEZMBcGA1UEAxMQQW1hem9uIFJvb3Qg
+Q0EgNDB2MBAGByqGSM49AgEGBSuBBAAiA2IABNKrijdPo1MN/sGKe0uoe0ZLY7Bi
+9i0b2whxIdIA6GO9mif78DluXeo9pcmBqqNbIJhFXRbb/egQbeOc4OO9X4Ri83Bk
+M6DLJC9wuoihKqB1+IGuYgbEgds5bimwHvouXKNCMEAwDwYDVR0TAQH/BAUwAwEB
+/zAOBgNVHQ8BAf8EBAMCAYYwHQYDVR0OBBYEFNPsxzplbszh2naaVvuc84ZtV+WB
+MAoGCCqGSM49BAMDA2gAMGUCMDqLIfG9fhGt0O9Yli/W651+kI0rz2ZVwyzjKKlw
+CkcO8DdZEv8tmZQoTipPNU0zWgIxAOp1AE47xDqUEpHJWEadIRNyp4iciuRMStuW
+1KyLa2tJElMzrdfkviT8tQp21KW8EA==
+-----END CERTIFICATE-----
+""";
+    client.httpClientAdapter = IOHttpClientAdapter(
+      createHttpClient: () {
+        final client = HttpClient();
+        client.badCertificateCallback = (X509Certificate cert, String host, int port) {
+          return cert.pem == pemString; // Verify the certificate.
+        };
+        return client;
+      },
+    );
   }
 }
