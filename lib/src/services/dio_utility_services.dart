8df3bc833d4672f71f968ef2ca4639575b7ba37a
Sarawut Saengchan <Sarawut.Saengchan@krungsri.com>
2023-11-28 09:50:57 +0700

    [SAMP-769][namo|art] Debug setupCertificate Function

--- lib/src/services/dio_utility_services.dart
+++ lib/src/services/dio_utility_services.dart
@@ -1,12 +1,15 @@
 import 'dart:convert';
 import 'dart:developer';
-import 'dart:io';
+
 
 import 'package:crypto/crypto.dart';
 import 'package:dio/dio.dart';
 import 'package:dio/io.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:marketplace_line_oa/src/helpers/dio_intercetptor.dart';
 import 'package:marketplace_line_oa/src/services/dio_utils/header_utils.dart';
+import 'package:universal_io/io.dart';
 
 class DioUtilityService with HeaderUtil {
   DioUtilityService({Dio? dio}) : _dioClient = dio ?? DioClient().dioClient;
@@ -85,7 +88,7 @@ class DioClient {
     client.interceptors.add(InterceptorsWrapper(
       onRequest: (RequestOptions options, RequestInterceptorHandler handler) async {
         // initAdapter();
-        // await setupCertificate();
+        await setupCertificate();
         return handler.next(options);
       },
     ));
@@ -111,59 +114,47 @@ class DioClient {
     );
   }
 
+  Future<Uint8List> loadAWSRootCA4Certificate(String pem) async {
+    final derCertificate = pem.codeUnits;
+    return Uint8List.fromList(derCertificate);
+  }
+
   Future<void> setupCertificate() async {
-    SecurityContext clientContext = SecurityContext(withTrustedRoots: false);
-//     const String awsRootCACertificate = '''
-// -----BEGIN CERTIFICATE-----
-// MIIB8jCCAXigAwIBAgITBmyf18G7EEwpQ+Vxe3ssyBrBDjAKBggqhkjOPQQDAzA5
-// MQswCQYDVQQGEwJVUzEPMA0GA1UEChMGQW1hem9uMRkwFwYDVQQDExBBbWF6b24g
-// Um9vdCBDQSA0MB4XDTE1MDUyNjAwMDAwMFoXDTQwMDUyNjAwMDAwMFowOTELMAkG
-// A1UEBhMCVVMxDzANBgNVBAoTBkFtYXpvbjEZMBcGA1UEAxMQQW1hem9uIFJvb3Qg
-// Q0EgNDB2MBAGByqGSM49AgEGBSuBBAAiA2IABNKrijdPo1MN/sGKe0uoe0ZLY7Bi
-// 9i0b2whxIdIA6GO9mif78DluXeo9pcmBqqNbIJhFXRbb/egQbeOc4OO9X4Ri83Bk
-// M6DLJC9wuoihKqB1+IGuYgbEgds5bimwHvouXKNCMEAwDwYDVR0TAQH/BAUwAwEB
-// /zAOBgNVHQ8BAf8EBAMCAYYwHQYDVR0OBBYEFNPsxzplbszh2naaVvuc84ZtV+WB
-// MAoGCCqGSM49BAMDA2gAMGUCMDqLIfG9fhGt0O9Yli/W651+kI0rz2ZVwyzjKKlw
-// CkcO8DdZEv8tmZQoTipPNU0zWgIxAOp1AE47xDqUEpHJWEadIRNyp4iciuRMStuW
-// 1KyLa2tJElMzrdfkviT8tQp21KW8EA==
-// -----END CERTIFICATE-----
-// ''';
-//     String certificateSHA256 = calculateSHA256(awsRootCACertificate);
-    String expectedSHA256 = 'd7c774cc0f66ce20d12628cd251a1b0982a31a2bf26735b017bd552d8ad2552d';
+    // final certificate = await loadAWSRootCA4Certificate(stringCertificateBytes);
+    final certificate =  calculateSHA256(stringCertificateBytes);
+    debugPrint("setupCertificate");
+    debugPrint("certificate $certificate");
     client.httpClientAdapter = IOHttpClientAdapter(
       createHttpClient: () {
-        HttpClient httpClient = HttpClient(context: clientContext);
-        httpClient.badCertificateCallback = (X509Certificate cert, String host, int port) {
-          if (calculateSHA256(cert.pem) == expectedSHA256) {
-            return true; // Accept the certificate
-          }
-          return false; // Reject the certificate
+        // final SecurityContext scontext = SecurityContext();
+        debugPrint("createHttpClient");
+        // scontext.setTrustedCertificatesBytes(certificate);
+        HttpClient client = HttpClient();
+        client.badCertificateCallback =
+            (X509Certificate cert, String host, int port) {
+              debugPrint("cert.pem : ${cert.pem}");
+              debugPrint("cert.pem SAH : ${calculateSHA256(cert.pem)}");
+              debugPrint("certificate.pem SAH : ${calculateSHA256(certificate)}");
+          return false; // Verify the certificate.
         };
-        return httpClient;
+        debugPrint("client $client");
+        return client;
       },
     );
-
-// static Future<void> _setupCertificate() async {
-    //   String path = 'assets/raw/AWSRootCA4.pem';
-    //   ByteData data = await rootBundle.load(path);
-    //   SecurityContext clientContext = SecurityContext(withTrustedRoots: false);
-    //   clientContext.setTrustedCertificatesBytes(data.buffer.asUint8List());
-    //   client.httpClientAdapter = IOHttpClientAdapter(
-    //     createHttpClient: () {
-    //       HttpClient httpClient = HttpClient(context: clientContext);
-    //       httpClient.badCertificateCallback = ((X509Certificate cert, String host, int port) => false);
-    //       return httpClient;
-    //     },
-    //   );
-    //
-    //   // old version code is notwork
-    //
-    //   // (client.httpClientAdapter as IOHttpClientAdapter).onHttpClientCreate = (client) {
-    //   //   HttpClient httpClient = HttpClient(context: clientContext);
-    //   //   httpClient.badCertificateCallback = (X509Certificate cert, String host, int port) {
-    //   //     return false;
-    //   //   };
-    //   //   return httpClient;
-    //   // };
   }
 }
+const stringCertificateBytes = '''
+-----BEGIN CERTIFICATE-----
+MIIB8jCCAXigAwIBAgITBmyf18G7EEwpQ+Vxe3ssyBrBDjAKBggqhkjOPQQDAzA5
+MQswCQYDVQQGEwJVUzEPMA0GA1UEChMGQW1hem9uMRkwFwYDVQQDExBBbWF6b24g
+Um9vdCBDQSA0MB4XDTE1MDUyNjAwMDAwMFoXDTQwMDUyNjAwMDAwMFowOTELMAkG
+A1UEBhMCVVMxDzANBgNVBAoTBkFtYXpvbjEZMBcGA1UEAxMQQW1hem9uIFJvb3Qg
+Q0EgNDB2MBAGByqGSM49AgEGBSuBBAAiA2IABNKrijdPo1MN/sGKe0uoe0ZLY7Bi
+9i0b2whxIdIA6GO9mif78DluXeo9pcmBqqNbIJhFXRbb/egQbeOc4OO9X4Ri83Bk
+M6DLJC9wuoihKqB1+IGuYgbEgds5bimwHvouXKNCMEAwDwYDVR0TAQH/BAUwAwEB
+/zAOBgNVHQ8BAf8EBAMCAYYwHQYDVR0OBBYEFNPsxzplbszh2naaVvuc84ZtV+WB
+MAoGCCqGSM49BAMDA2gAMGUCMDqLIfG9fhGt0O9Yli/W651+kI0rz2ZVwyzjKKlw
+CkcO8DdZEv8tmZQoTipPNU0zWgIxAOp1AE47xDqUEpHJWEadIRNyp4iciuRMStuW
+1KyLa2tJElMzrdfkviT8tQp21KW8EA==
+-----END CERTIFICATE-----
+''';
