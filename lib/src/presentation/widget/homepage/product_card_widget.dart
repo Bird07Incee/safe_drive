c33e1d645d6447ee36f5f6672033918a1ade1ea1
Prem.natthapong <natthapong.tantecho@krungsri.com>
2024-01-12 10:29:56 +0700

    [SAMP-X][Prem]Update productDetail merchantLogo load from image network url2

--- lib/src/presentation/widget/homepage/product_card_widget.dart
+++ lib/src/presentation/widget/homepage/product_card_widget.dart
@@ -189,11 +189,11 @@ class ProductCardWidget extends StatelessWidget {
                                     Positioned.fill(
                                         child: Align(
                                       alignment: Alignment.bottomRight,
-                                      child: Image.asset(
-                                        "assets/homepage/brand.png",
+                                      child: Image.network(
+                                        products[index].merchantLogo,
                                         height: 32,
                                         fit: BoxFit.cover,
-                                        errorBuilder: (context, error, stackTrace) => const SizedBox(),
+                                        errorBuilder: (context, error, stackTrace) => const SizedBox.shrink(),
                                       ),
                                     )),
                                     Visibility(
