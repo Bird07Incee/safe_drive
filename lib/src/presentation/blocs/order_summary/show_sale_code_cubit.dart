26aa7e46c5f68b0b1ff724168a94b26c987cdd6c
chanitasukto <70008797@krungsri.com>
2024-03-05 15:24:02 +0700

    [SAMP-921][Ploy] Hide sale code on Order summary screen

--- lib/src/presentation/blocs/order_summary/show_sale_code_cubit.dart
+++ lib/src/presentation/blocs/order_summary/show_sale_code_cubit.dart
@@ -1,10 +1,10 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class ShowSaleCodeCubit extends Cubit<bool> {
-  ShowSaleCodeCubit() : super(false);
-  static ShowSaleCodeCubit get(context) => BlocProvider.of(context);
-
-  void show(bool show) {
-    emit(show);
-  }
-}
+// class ShowSaleCodeCubit extends Cubit<bool> {
+//   ShowSaleCodeCubit() : super(false);
+//   static ShowSaleCodeCubit get(context) => BlocProvider.of(context);
+//
+//   void show(bool show) {
+//     emit(show);
+//   }
+// }
