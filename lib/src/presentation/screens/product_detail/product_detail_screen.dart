29c7ad6b84334045983a0c70dfd34a58d0dd4a80
Nuttawut <thisnattawut@gmail.com>
2024-03-15 16:56:36 +0700

    [SAMP-948][web1.0][Pung] change onTap

--- lib/src/presentation/screens/product_detail/product_detail_screen.dart
+++ lib/src/presentation/screens/product_detail/product_detail_screen.dart
@@ -297,7 +297,6 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> with TickerPr
   Widget viewImagePage(
       PageController pageController, BuildContext context, TransformationController zoomState, double previousState, ProductDetailState pdState) {
     backButtontoDetail() {
-      showOneTrustCookieScript();
       final myBloc = BlocProvider.of<ProductDetailDescriptionCubit>(context);
 
       myBloc.updateToggleTapDescription(toggleDescription: false);
