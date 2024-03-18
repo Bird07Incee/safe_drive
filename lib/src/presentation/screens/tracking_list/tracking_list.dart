47b08158bed0820f9bd55ca43728a12e0ead7c56
Nuttawut <thisnattawut@gmail.com>
2024-03-18 17:04:47 +0700

    [SAMP-948][web1.0][Pung] change hide onetrust logic

--- lib/src/presentation/screens/tracking_list/tracking_list.dart
+++ lib/src/presentation/screens/tracking_list/tracking_list.dart
@@ -27,7 +27,6 @@ class _TrackingListScreenState extends State<TrackingListScreen> {
   @override
   void initState() {
     super.initState();
-    hideOneTrustCookieScript();
     context.read<TrackingOrderBloc>().add(GetTrackingOrderListByPage(1, context));
   }
 
@@ -67,6 +66,7 @@ class _TrackingListScreenState extends State<TrackingListScreen> {
         child: BlocBuilder<TrackingOrderBloc, TrackingOrderState>(
           builder: (context, state) {
             if (state.trackingOrderListStatus == GetTrackingOrderListStatus.success) {
+              hideOneTrustCookieScript();
               return AlvaRootWidget(
                   titlePage: titleWebPage,
                   appBar: appBar,
