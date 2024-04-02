d19b342803bef8656b7421ca3a4053f5d84ce66f
Prem.natthapong <natthapong.tantecho@krungsri.com>
2024-04-02 16:59:30 +0700

    [SAMP-907][Prem]Add new landing screen to test deeplink to prompt buy mobile

---
+++ lib/src/presentation/screens/landing_screen.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:marketplace_line_oa/src/presentation/widget/alva_c_p_i_loader.dart';
+import 'package:marketplace_line_oa/src/routes/routing_data.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class LandingScreen extends StatefulWidget {
+  const LandingScreen({super.key});
+
+  @override
+  State<LandingScreen> createState() => _LandingScreenState();
+}
+
+class _LandingScreenState extends State<LandingScreen> {
+  late RouteSettings? settings;
+  String redirectUrl = "";
+
+  _redirectTo(String url) {
+    Future.delayed(const Duration(seconds: 5)).then((r) {
+      print('launch url: $url');
+      try {
+        launchUrl(Uri.parse(url));
+      } catch (e) {
+        print("launch fail: $e");
+      }
+
+    });
+  }
+
+  _loadSetting() {
+    try {
+      settings = ModalRoute.of(context) != null ? ModalRoute.of(context)!.settings : null;
+      if (settings != null) {
+        var uriData = Uri.parse(settings!.name!);
+        var routingData = RoutingData(route: uriData.path, queryParameters: uriData.queryParameters);
+        redirectUrl = (routingData["redirectUrl"] == null) ? "" : routingData["redirectUrl"];
+        if (redirectUrl.isNotEmpty) {
+          String urlDecoded = Uri.decodeFull(redirectUrl);
+          _redirectTo(urlDecoded);
+        }
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    _loadSetting();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double maxWidth = MediaQuery.of(context).size.width;
+    double maxHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: Container(
+        width: maxWidth,
+        height: maxHeight,
+        color: Colors.white,
+        child: Center(
+          child: GestureDetector(
+            onDoubleTap: () {
+              String urlDecoded = Uri.decodeFull(redirectUrl);
+              launchUrl(Uri.parse(urlDecoded));
+            },
+              child: AlvaCPILoader()
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:marketplace_line_oa/src/presentation/screens/error_screen.dart';
 import 'package:marketplace_line_oa/src/presentation/screens/home.dart';
+import 'package:marketplace_line_oa/src/presentation/screens/landing_screen.dart';
 import 'package:marketplace_line_oa/src/presentation/screens/loading_screen.dart';
 import 'package:marketplace_line_oa/src/presentation/screens/order_cancel.dart';
 import 'package:marketplace_line_oa/src/presentation/screens/order_success.dart';
@@ -160,7 +161,9 @@ Route<dynamic> generateRoute(RouteSettings settings) {
       return MaterialPageRoute(builder: (_) => RefundFormTrackingScreen(), settings: settings);
     case "/refundSuccess":
       return MaterialPageRoute(builder: (_) => RefundSuccessScreen(), settings: settings);
+    case "/landing":
+      return MaterialPageRoute(builder: (_) => LandingScreen(), settings: settings);
     default:
-      return MaterialPageRoute(builder: (_) => const HomeScreen(), settings: settings);
+      return MaterialPageRoute(builder: (_) => const LandingScreen(), settings: settings);
   }
 }
