6eff86c36674dc65bf598afdb6d368af15ce8288
kanphop <kanphob.kaenchaleaw@krungsri.com>
2024-04-09 18:22:12 +0700

    [SAMP-X][Kan]Fix unit test failed

--- test/unit_test/blocs/refund_request_bloc/refund_request_bloc_test.dart
+++ test/unit_test/blocs/refund_request_bloc/refund_request_bloc_test.dart
@@ -68,8 +68,8 @@ void main() {
         setUp: () async {
           SharedPreferences.setMockInitialValues({});
           final baseUrl = Environment().getValue("BFF_BASE_URL");
-          final transactionApiPath = Environment().getValue("BFF_TRANSACTION_INQUIRY_BASE_URL");
-          String path = "/v1/inquiry";
+          final transactionApiPath = Environment().getValue("BFF_TRANSACTION_REFUND_BASE_URL");
+          String path = "/v1/inquiry/refund";
           final mock = {"uid": "1234"};
           await LineDataHelper().saveSocialDataToLocalStorage(json.encode(mock));
           when(() {
