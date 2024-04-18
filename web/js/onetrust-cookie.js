fb665f6bc41076d7d886602b9c6a6efa1d2ba067
Phongphun Thuptimtian (BAY Auto) <Phongphun.Thuptimtian@krungsri.com>
2024-04-18 21:01:37 +0700

    [SAMP-X][Tong][PromptBuy-Web] change the onetrust script for production

--- web/js/onetrust-cookie.js
+++ web/js/onetrust-cookie.js
@@ -1,7 +1,7 @@
 function loadOneTrustCookieScript() {
     let script1 = document.createElement('script');
     script1.type = "text/javascript";
-    script1.src = "https://cdn-apac.onetrust.com/consent/5304c4a0-db56-49ce-9950-fe9f7ac9aedc-test/OtAutoBlock.js";
+    script1.src = "https://cdn-apac.onetrust.com/consent/018ef16a-cf99-7e16-85e4-2ac03c9aed2b/OtAutoBlock.js";
     document.body.appendChild(script1);
 
     let script2 = document.createElement('script');
@@ -9,7 +9,7 @@ function loadOneTrustCookieScript() {
     script2.src = "https://cdn-apac.onetrust.com/scripttemplates/otSDKStub.js";
     script2.setAttribute("charset", "UTF-8");
     script2.setAttribute("data-document-language", "true");
-    script2.setAttribute("data-domain-script", "5304c4a0-db56-49ce-9950-fe9f7ac9aedc-test");
+    script2.setAttribute("data-domain-script", "018ef16a-cf99-7e16-85e4-2ac03c9aed2b");
     document.body.appendChild(script2);
 }
 
