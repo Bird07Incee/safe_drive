97469152a29858b334bebc516e1614558f74cbc0
Phongphun Thuptimtian (BAY Auto) <Phongphun.Thuptimtian@krungsri.com>
2024-04-22 00:12:38 +0700

    [SAMP-X][Tong][PromptBuy-Web] test with car4cash and PromptBut (Not use distinct script src URL)

--- web/js/onetrust-cookie.js
+++ web/js/onetrust-cookie.js
@@ -2,23 +2,23 @@ function loadOneTrustCookieScript(env) {
     let script1 = document.createElement('script');
     script1.type = "text/javascript";
     if(env === "prod") {
-    script1.src = "https://cdn-apac.onetrust.com/consent/018ef16a-cf99-7e16-85e4-2ac03c9aed2b/OtAutoBlock.js";
+    script1.src = "https://cdn-apac.onetrust.com/consent/fd88598f-bb24-4652-bd5c-098c1b1446eb/OtAutoBlock.js";
     } else {
-    script1.src = "https://cdn-apac.onetrust.com/consent/018ef16a-cf99-7e16-85e4-2ac03c9aed2b-test/OtAutoBlock.js";
+    script1.src = "https://cdn-apac.onetrust.com/consent/fd88598f-bb24-4652-bd5c-098c1b1446eb-test/OtAutoBlock.js";
     }
     document.body.appendChild(script1);
 
     let script2 = document.createElement('script');
     script2.type = "text/javascript";
-    script2.src = "https://cdn-apac.onetrust.com/consent/018ef16a-cf99-7e16-85e4-2ac03c9aed2b/otSDKStub.js";
+    script2.src = "https://cdn-apac.onetrust.com/scripttemplates/otSDKStub.js";
     script2.setAttribute("data-language", "th");
     script2.setAttribute("charset", "UTF-8");
     script2.setAttribute("data-document-language", "true");
 
     if(env === "prod") {
-    script2.setAttribute("data-domain-script", "018ef16a-cf99-7e16-85e4-2ac03c9aed2b");
+    script2.setAttribute("data-domain-script", "fd88598f-bb24-4652-bd5c-098c1b1446eb");
     } else {
-    script2.setAttribute("data-domain-script", "018ef16a-cf99-7e16-85e4-2ac03c9aed2b-test");
+    script2.setAttribute("data-domain-script", "fd88598f-bb24-4652-bd5c-098c1b1446eb-test");
     }
     document.body.appendChild(script2);
 }
