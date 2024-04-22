59c93667555834fc07d73ef9fd68f04f19acf21f
kanphop <kanphob.kaenchaleaw@krungsri.com>
2024-04-22 16:25:22 +0700

    [SAMP-996][Kan]Fix unit test failed

--- test/unit_test/extensions/number_converter_extension_test.dart
+++ test/unit_test/extensions/number_converter_extension_test.dart
@@ -3,7 +3,7 @@ import 'package:marketplace_line_oa/src/extension/number_converter.dart';
 
 void main() {
   test('Should return string as the decimal number formatted', () {
-    String expectedStr = '1,000,000';
+    String expectedStr = '1,000,000.00';
     double number = 1000000;
     expect(number.toDecimalFormat(), expectedStr);
   });
