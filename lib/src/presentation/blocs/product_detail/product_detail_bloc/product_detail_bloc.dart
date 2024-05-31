9935075ff000fda98b478353427ee97957f5b646
kanphop <kanphob.kaenchaleaw@krungsri.com>
2024-05-31 19:01:27 +0700

    [SAMP-X][Kan] fix ma create object get from storage

--- lib/src/presentation/blocs/product_detail/product_detail_bloc/product_detail_bloc.dart
+++ lib/src/presentation/blocs/product_detail/product_detail_bloc/product_detail_bloc.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -27,7 +29,8 @@ class ProductDetailBloc extends Bloc<ProductDetailEvent, ProductDetailState> {
       String path = "/ecommerce/v1/products";
       Response response = await utilityRepository.getByURL("$baseUrl$inventoryApiPath$path", {"pid": event.pid});
       final p = Product.fromJson(response.data);
-      MaintenanceHelper().saveMaintenanceDataToLocalStorage(p.serviceMa.toString());
+      var jsonString = jsonEncode(p.serviceMa);
+      MaintenanceHelper().saveMaintenanceDataToLocalStorage(jsonString);
       emit(state.copyWith(status: p == Product.empty ? ProductDetailStatus.error : ProductDetailStatus.success, product: p));
     } catch (e) {
       emit(state.copyWith(status: ProductDetailStatus.error));
