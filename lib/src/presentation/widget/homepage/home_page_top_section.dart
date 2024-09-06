bd896fe11653bbc86b53573113eeeb454a5321fa
Sarawut Saengchan <Sarawut.Saengchan@krungsri.com>
2024-09-06 13:57:58 +0700

    [SAMP-x][nsmo] fix check

--- lib/src/presentation/widget/homepage/home_page_top_section.dart
+++ lib/src/presentation/widget/homepage/home_page_top_section.dart
@@ -7,7 +7,6 @@ import 'package:marketplace_line_oa/src/helpers/amplitude_web_helper.dart';
 import 'package:marketplace_line_oa/src/js/js_manager.dart';
 import 'package:marketplace_line_oa/src/presentation/blocs/product_list/product_list_bloc.dart';
 import 'package:marketplace_line_oa/src/presentation/widget/alva_text.dart';
-import 'package:marketplace_line_oa/src/routes/routes.dart';
 
 class HomepageTopSection extends StatelessWidget {
   const HomepageTopSection({super.key, required this.maxWidth, required this.scrollController});
