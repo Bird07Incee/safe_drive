45d94cbd5281ec2722111d6750bba9f5dfa9a589
Sarawut Saengchan <Sarawut.Saengchan@krungsri.com>
2024-09-06 14:19:49 +0700

    [SAMP-x][nsmo] back to parent page log amplitude round 3

--- lib/src/presentation/widget/homepage/home_page_top_section.dart
+++ lib/src/presentation/widget/homepage/home_page_top_section.dart
@@ -7,6 +7,7 @@ import 'package:marketplace_line_oa/src/helpers/amplitude_web_helper.dart';
 import 'package:marketplace_line_oa/src/js/js_manager.dart';
 import 'package:marketplace_line_oa/src/presentation/blocs/product_list/product_list_bloc.dart';
 import 'package:marketplace_line_oa/src/presentation/widget/alva_text.dart';
+import 'package:marketplace_line_oa/src/routes/routes.dart';
 
 class HomepageTopSection extends StatelessWidget {
   const HomepageTopSection({super.key, required this.maxWidth, required this.scrollController});
@@ -36,12 +37,15 @@ class HomepageTopSection extends StatelessWidget {
                     ],
                   ),
                   GestureDetector(
-                    onTap: () {
+                    onTap: () async {
                       AmplitudeWebHelper.getInstance().logTapOnOrderTrackingButton();
                       hideOneTrustCookieScript();
                       context.read<ProductListBloc>().add(SetScrollPosition(scrollController.offset));
-                      Navigator.pushNamed(context, '/trackingList').then((value) {
-                        AmplitudeWebHelper.getInstance().logeMarketplaceHomePageHomeScreen();
+                      await Navigator.pushNamed(context, '/trackingList').then((value) {
+                        if (ModalRoute.of(context)!.settings.name! == Routes.initial.toStringPath()) {
+                          AmplitudeWebHelper amplitudeWebHelper = AmplitudeWebHelper.getInstance();
+                          amplitudeWebHelper.logeMarketplaceHomePageHomeScreen();
+                        }
                       });
                     },
                     child: Container(
