08dff50b82c95478909cba35ce271ce9cf97377f
Sarawut Saengchan <Sarawut.Saengchan@krungsri.com>
2024-09-06 10:49:40 +0700

    [SAMP-x][nsmo] test log round 2

--- lib/src/presentation/screens/home.dart
+++ lib/src/presentation/screens/home.dart
@@ -25,6 +25,7 @@ import 'package:marketplace_line_oa/src/presentation/widget/homepage/home_page_b
 import 'package:marketplace_line_oa/src/presentation/widget/homepage/home_page_top_section.dart';
 import 'package:marketplace_line_oa/src/presentation/widget/homepage/product_card_widget.dart';
 import 'package:marketplace_line_oa/src/presentation/widget/root_widget.dart';
+import 'package:marketplace_line_oa/src/presentation/widget/snackbar/mkp_toast.dart';
 import 'package:marketplace_line_oa/src/routes/routes.dart';
 import 'package:sticky_headers/sticky_headers.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -69,7 +70,7 @@ class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
     if (!tc && CurrentRouteObserver.instance.last != Routes.termAndCon.toStringPath()) {
       nav.pushNamed(Routes.termAndCon.toStringPath());
     } else {
-      print("route ${route!.settings.name!}");
+      ScaffoldMessenger.of(context).showSnackBar(getMkpToast("route ${route!.settings.name!}"));
       if (route!.settings.name! == Routes.initial.toStringPath()) {
         amplitudeWebHelper.logeMarketplaceHomePageHomeScreen();
       }
