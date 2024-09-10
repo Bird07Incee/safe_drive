823a1485b6271ec940785a852e197306f0ae2f29
kanphop <kanphob.kaenchaleaw@krungsri.com>
2024-09-10 16:14:01 +0700

    [SAMP-1241][Kan, Namo] fix amplitude duplicated finish#3

--- lib/main.dart
+++ lib/main.dart
@@ -24,15 +24,14 @@ void main() async {
   setUrlStrategy(urlStrategyPromptBuy);
   WidgetsFlutterBinding.ensureInitialized();
   _configureApp();
-  _setUpAmplitude();
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]).then((_) async {
-    runApp(MyApp());
-    // DatadogSdk.runApp(configuration, TrackingConsent.granted, () async {
-    //   return runApp(const MyApp());
-    // });
+    // runApp(MyApp());
+    DatadogSdk.runApp(configuration, TrackingConsent.granted, () async {
+      return runApp(const MyApp());
+    });
   });
 }
 
