9a5bd865212d6ddbadb05cc45c6f2887d2d1e125
Sarawut Saengchan <Sarawut.Saengchan@krungsri.com>
2024-10-24 14:27:36 +0700

    [SAMP-1284][nsmo] fix log data dog to rum

--- lib/src/presentation/blocs/log_activity_bloc/log_activity_bloc.dart
+++ lib/src/presentation/blocs/log_activity_bloc/log_activity_bloc.dart
@@ -28,6 +28,7 @@ class LogActivityBloc extends Bloc<LogActivityEvent, LogActivityState> {
       Response response = await utilityRepository.postByURL("$baseUrl$socialPath$logActivityPath", event.payload);
       if (response.statusCode == 200) {
         final ActivityLogResponseModel responseData = ActivityLogResponseModel.fromJson(response.data);
+        DatadogSdk.instance.rum?.addAction(RumActionType.custom, "activity log Bloc Status Response: ${responseData.status}");
         emit(LogActivitySuccess(responseData));
       } else {
         final ActivityLogResponseModel responseData = ActivityLogResponseModel.fromJson({
